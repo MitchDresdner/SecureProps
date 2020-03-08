9cf517312ecb71f2887f6d04f6cf34ab84e0ee03
M. Dresdner <mitchd@mindspring.com>
2020-03-08 07:26:52 -0400

    moved images folder into resources

--- src/main/kotlin/com/example/demo/app/MyApp.kt
+++ src/main/kotlin/com/example/demo/app/MyApp.kt
@@ -5,6 +5,7 @@ import javafx.scene.image.Image
 import javafx.stage.Stage
 import tornadofx.*
 import java.nio.file.Paths
+import javax.imageio.ImageIO
 
 class MyApp: App(MainView::class, Styles::class) {
     override fun start(stage: Stage) {
@@ -14,7 +15,10 @@ class MyApp: App(MainView::class, Styles::class) {
         }
 
         var myPath = Paths.get("").toAbsolutePath().toString()
-        setStageIcon(Image("file:///${myPath}/images/Tardis-133x200.jpg"))      // Favico.png
+        //setStageIcon(Image("file:///${myPath}/images/Tardis-133x200.jpg"))      // Favico.png
+        // Good ./images: setStageIcon(Image("file:images/Tardis-133x200.jpg"))
+        //val image = ImageIO.read(javaClass.getResource("images/Tardis-133x200.jpg").toString())
+        setStageIcon(Image("images/Tardis-133x200.jpg"))
 
         super.start(stage)
     }
